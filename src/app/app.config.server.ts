d4411955eefc7d7bffb98ec1ae824393f82625e9
Robert Carrillo <robertcl795.rc@gmail.com>
2025-03-11 18:20:33 -0600

    feat: PokeSSR V1.0.1

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -5,7 +5,7 @@ import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
