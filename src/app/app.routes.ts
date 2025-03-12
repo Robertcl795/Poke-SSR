fba79b7aaf6db51ec53690e030ac9d83fb8efb14
Robert Carrillo <robertcl795.rc@gmail.com>
2025-03-11 21:50:14 -0600

    feat: V1.1 - SSR and SSG

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'pokemons/page/1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pokemons/page/:page',
     loadComponent: () => import('./pages/pokemons/pokemons-page.component'),
   },
   {
     path: 'pokemons/:id',
     loadComponent: () => import('./pages/pokemon-details/pokemon-details.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'pokemons/page/1',
   },
 ];
--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,15 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import { PokemonListComponent } from '../../shared/components/pokemon-list/pokemon-list.component';
 import { PokemonListSkeletonComponent } from '../../shared/components/pokemon-list-skeleton/pokemon-list-skeleton.component';
 import { PokemonsService } from '../../shared/services/pokemon.service';
 import { SimplePokemon } from '../../shared/interfaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map, tap } from 'rxjs';
 @Component({
   selector: 'app-pokemons-page',
-  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent, RouterLink],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -18,55 +18,32 @@ export default class PokemonsPageComponent {
   public pokemons = signal<SimplePokemon[]>([]);
 
   private route = inject(ActivatedRoute);
-  private router = inject(Router);
+
   private title = inject(Title);
 
   public currentPage = toSignal<number>(
-    this.route.queryParamMap.pipe(
-      map((params) => params.get('page') ?? '1'),
+    this.route.params.pipe(
+      map((params) => params['page'] ?? '1'),
       map((page) => (isNaN(+page) ? 1 : +page)),
       map((page) => Math.max(1, page))
     )
   );
 
-  // public isLoading = signal(true);
-
-  // private appRef = inject(ApplicationRef);
-
-  // private $appState = this.appRef.isStable.subscribe((isStable) => {
-  //   console.log({ isStable });
-  // });
-
-  ngOnInit(): void {
-    // this.route.queryParamMap.subscribe(console.log);
-    console.log(this.currentPage());
-
-    this.loadPokemons();
-    // title
-    // Meta-tags
-    // Stable
-    // setTimeout(() => {
-    //   this.isLoading.set(false);
-    // }, 5000);
-  }
+  public loadOnPageChanged = effect(
+    () => {
+      this.loadPokemons(this.currentPage());
+    },
+    {
+      allowSignalWrites: true,
+    }
+  );
 
   public loadPokemons(page = 0) {
-    const pageToLoad = this.currentPage()! + page;
-
-    // console.log({ pageToLoad, currentPage: this.currentPage() });
-
     this.pokemonsService
-      .loadPage(pageToLoad)
-      .pipe(
-        tap(() => this.router.navigate([], { queryParams: { page: pageToLoad } })),
-        tap(() => this.title.setTitle(`Pokémons SSR - Page ${pageToLoad}`))
-      )
+      .loadPage(page)
+      .pipe(tap(() => this.title.setTitle(`Pokémons SSR - Page ${page}`)))
       .subscribe((pokemons) => {
         this.pokemons.set(pokemons);
       });
   }
-
-  // ngOnDestroy(): void {
-  //   this.$appState.unsubscribe();
-  // }
 }
